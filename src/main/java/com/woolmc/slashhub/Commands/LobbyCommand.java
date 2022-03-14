94c7fdb4bc50740ef07f8431801ab7384f0b0c96
block of grass <64753497+skygamezmc@users.noreply.github.com>
2022-03-14 17:50:23 -0500

    Update LobbyCommand.java

--- src/main/java/com/woolmc/slashhub/Commands/LobbyCommand.java
+++ src/main/java/com/woolmc/slashhub/Commands/LobbyCommand.java
@@ -18,27 +18,31 @@ public class LobbyCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (sender instanceof ProxiedPlayer) {
-            config = Main.config;
-            ProxiedPlayer p = (ProxiedPlayer)sender;
-            ProxiedPlayer target = ProxyServer.getInstance().getPlayer(sender.getName());
-            if(sender.hasPermission("slashhub.use")) {
-                if(ProxyServer.getInstance().getConfig().getServers().containsKey(config.getString("TargetServer"))) {
-                    if (!target.getServer().getInfo().getName().equals(config.getString("TargetServer"))) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("ConnectingMessage")));
-                        p.connect(ProxyServer.getInstance().getServerInfo(config.getString("TargetServer")));
-                    } else {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("AlreadyOnHubMessage")));
-                    }
-                } else {
-                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("ServerNotFound")));
-                    ProxyServer.getInstance().getLogger().warning("[SlashHub] Player " + sender.getName() + " has attempted to connect to server " + config.getString("TargetServer") + " However this server was not found in your Bungeecord Config");
-                }
-            } else {
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("NoPermission")));
-            }
-        } else {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("CannotExecuteOnConsoleMessage")));
+        config = Main.config;
+        if (!(sender instanceof ProxiedPlayer)) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.CannotExecuteOnConsoleMessage")));
+            return;
         }
+
+        if(!sender.hasPermission("slashhub.use")) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.NoPermission")));
+            return;
+        }
+        ProxiedPlayer p = (ProxiedPlayer)sender;
+        ProxiedPlayer target = ProxyServer.getInstance().getPlayer(sender.getName());
+
+        if(!ProxyServer.getInstance().getConfig().getServers().containsKey(config.getString("TargetServer"))) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.ServerNotFound")));
+            ProxyServer.getInstance().getLogger().warning("[SlashHub] Player " + sender.getName() + " has attempted to connect to server " + config.getString("TargetServer") + " However this server was not found in your Bungeecord Config");
+            return;
+        }
+
+        if (target.getServer().getInfo().getName().equals(config.getString("TargetServer"))) {
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.AlreadyOnHubMessage")));
+            return;
+        }
+
+        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("Messages.ConnectingMessage")));
+        p.connect(ProxyServer.getInstance().getServerInfo(config.getString("TargetServer")));
     }
 }
